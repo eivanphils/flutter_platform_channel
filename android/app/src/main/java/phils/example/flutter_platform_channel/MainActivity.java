fe3d10fc6e6843f030c16daacc0a48b32dadc4ba
Phils Garcia <eivanphils@gmail.com>
2024-04-24 10:49:08 -0400

    Agregado methodChannel para consultar la version del android

--- android/app/src/main/java/phils/example/flutter_platform_channel/MainActivity.java
+++ android/app/src/main/java/phils/example/flutter_platform_channel/MainActivity.java
@@ -1,6 +1,37 @@
 package phils.example.flutter_platform_channel;
 
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+
+        BinaryMessenger binaryMessenger = flutterEngine.getDartExecutor().getBinaryMessenger();
+
+        // se crea la instacion de methodChannel para escuchar el mensaje del cliente(flutter)
+        MethodChannel methodChannel = new MethodChannel(binaryMessenger, "phils.examples/android_channel");
+        methodChannel.setMethodCallHandler((MethodCall call, MethodChannel.Result result) -> {
+            if(call.method.equals("getVersion")) {
+                String version = getAndroidVersion();
+                result.success(version);
+            } else {
+                result.notImplemented();
+            }
+        });
+    }
+
+    String getAndroidVersion() {
+        int sdkVersion = Build.VERSION.SDK_INT;
+        String release = Build.VERSION.RELEASE;
+        return "Android version "+sdkVersion+" ("+release+")";
+    }
 }
--- lib/platforms/android_channel.dart
+++ lib/platforms/android_channel.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/services.dart';
 
 class AndroidChannel {
+  // se crea instancia de methodChannel
+  // se agrega el identificar que debe ser usado en ios/android
   final MethodChannel _methodChannel =
       const MethodChannel('phils.examples/android_channel');
 
+  // Definicion de metodo que sera llamado desde mi codigo nativo
   Future<void> getVersion() async {
-    final version = await _methodChannel.invokeMethod('getVersion');
-    print('version $version');
+    try {
+      final version = await _methodChannel.invokeMethod('getVersion');
+      print('version $version');
+    } catch (e) {
+      print('error al llamar al methodChannel $e');
+    }
   }
 }
